6cd7ef6f7aea175fcab47fe630e3376a10112849
Ryan Hill <r.l.hill128@gmail.com>
2022-10-13 17:12:35 +0100

    version 0.0.1

---
+++ macros/split_events.sql
@@ -0,0 +1,151 @@
+{% macro split_events(event_name, flat_cols = [], sde_col = '', sde_keys = [], sde_types = [], context_cols = [], context_keys = [], context_types = [], context_aliases = []) %}
+    {{ return(adapter.dispatch('split_events')(event_name, flat_cols, sde_col, sde_keys, sde_types, context_cols, context_keys, context_types, context_aliases)) }}
+{% endmacro %}
+
+{% macro snowflake__split_events(event_name, flat_cols, sde_col, sde_keys, sde_types, context_cols, context_keys, context_types, context_aliases) %}
+
+{# Remove down to major version for Snowflake columns, drop 2 last _X values #}
+{%- set sde_col = '_'.join(sde_col.split('_')[:-2]) -%} 
+{%- set context_cols_clean = [] -%}
+{%- for ind in range(context_cols|length) -%}
+    {% do context_cols_clean.append('_'.join(context_cols[ind].split('_')[:-2])) -%} 
+{%- endfor -%}
+
+select
+    event_id
+    , collector_tstamp
+    -- Flat columns from event table
+    {% if flat_cols|length > 0 %}
+    {%- for col in flat_cols -%}
+    , {{ col }}
+    {% endfor -%}
+    {%- endif -%}
+    -- self describing events column from event table
+    {% if sde_col != '' %}
+    {%- for key, type in zip(sde_keys, sde_types) -%}
+    , {{ sde_col }}:{{ key }}::{{ type }} as {{ key }}
+    {% endfor -%}
+    {%- endif -%}
+    -- context column(s) from the event table
+    {% if context_cols_clean|length > 0 %}
+    {%- for col, col_ind in zip(context_cols_clean, range(context_cols_clean|length)) -%}
+    {%- for key, type in zip(context_keys[col_ind], context_types[col_ind]) -%}
+    {% if context_aliases|length > 0 -%}
+    , {{ col }}[0]:{{ key }}::{{ type }} as {{ context_aliases[col_ind] }}_{{ key }}
+    {% else -%}
+    , {{ col }}[0]:{{ key }}::{{ type }} as {{ key }}
+    {%- endif -%}
+    {%- endfor -%}
+    {%- endfor -%}
+    {%- endif %}
+from
+    {{ ref('snowplow_web_base_events_this_run') }}
+where
+    event_name = '{{ event_name }}'
+    and {{ snowplow_utils.is_run_with_new_events("snowplow_web") }}
+{% endmacro %}
+
+
+{% macro bigquery__split_events(event_name, flat_cols, sde_col, sde_keys, sde_types, context_cols, context_keys, context_types, context_aliases) %}
+{# Replace keys with snake_case where needed #}
+{% set re = modules.re %}
+{% set camel_string = '(?<!^)(?=[A-Z])'%}
+{%- set sde_keys_clean = [] -%} 
+{%- set context_keys_clean = [] -%}
+{%- for ind in range(sde_keys|length) -%}
+    {% do sde_keys_clean.append(re.sub(camel_string, '_', sde_keys[ind]).lower()) -%} 
+{%- endfor -%}
+{%- for ind1 in range(context_keys|length) -%}
+    {%- set context_key_clean = [] -%}
+    {%- for ind2 in range(context_keys[ind1]|length) -%}
+        {% do context_key_clean.append(re.sub(camel_string, '_', context_keys[ind1][ind2]).lower()) -%} 
+    {%- endfor -%}
+    {% do context_keys_clean.append(context_key_clean) -%} 
+{%- endfor -%}
+
+
+select
+    event_id
+    , collector_tstamp
+    -- Flat columns from event table
+    {% if flat_cols|length > 0 %}
+    {%- for col in flat_cols -%}
+    , {{ col }}
+    {% endfor -%}
+    {%- endif -%}
+    -- self describing events column from event table
+    {% if sde_col != '' %}
+    {%- for key, type in zip(sde_keys_clean, sde_types) -%}
+    , {{ sde_col }}.{{ key }} as {{ key }}
+    {% endfor -%}
+    {%- endif -%}
+    -- context column(s) from the event table
+    {% if context_cols|length > 0 %}
+    {%- for col, col_ind in zip(context_cols, range(context_cols|length)) -%}
+    {%- for key in context_keys_clean[col_ind] -%}
+    {% if context_aliases|length > 0 -%}
+    , {{ col }}[SAFE_OFFSET(0)].{{ key }} as {{ context_aliases[col_ind] }}_{{ key }}
+    {% else -%}
+    , {{ col }}[SAFE_OFFSET(0)].{{ key }} as {{ key }}
+    {%- endif -%}
+    {%- endfor -%}
+    {%- endfor -%}
+    {%- endif %}
+from
+    {{ ref('snowplow_web_base_events_this_run') }}
+where
+    event_name = '{{ event_name }}'
+    and {{ snowplow_utils.is_run_with_new_events("snowplow_web") }}
+{% endmacro %}
+
+{# {% macro databricks__split_events(event_name, flat_cols, sde_col, sde_keys, sde_types, context_cols, context_keys, context_types, context_aliases) %}
+
+{# Replace keys with snake_case where needed #}
+{% set re = modules.re %}
+{% set camel_string = '(?<!^)(?=[A-Z])'%}
+{%- set sde_keys_clean = [] -%} 
+{%- set context_keys_clean = [] -%}
+{%- for ind in range(sde_keys|length) -%}
+    {% do sde_keys_clean.append(re.sub(camel_string, '_', sde_keys[ind]).lower()) -%} 
+{%- endfor -%}
+{%- for ind1 in range(context_keys|length) -%}
+    {%- set context_key_clean = [] -%}
+    {%- for ind2 in range(context_keys[ind1]|length) -%}
+        {% do context_key_clean.append(re.sub(camel_string, '_', context_keys[ind1][ind2]).lower()) -%} 
+    {%- endfor -%}
+    {% do context_keys_clean.append(context_key_clean) -%} 
+{%- endfor -%}
+
+select
+    event_id
+    , collector_tstamp
+    -- Flat columns from event table
+    {% if flat_cols|length > 0 %}
+    {%- for col in flat_cols -%}
+    , {{ col }}
+    {% endfor -%}
+    {%- endif -%}
+    -- self describing events column from event table
+    {% if sde_col != '' %}
+    {%- for key, type in zip(sde_keys, sde_types) -%}
+    , {{ sde_col }}.{{ key }} as {{ key }}
+    {% endfor -%}
+    {%- endif -%}
+    -- context column(s) from the event table
+    {% if context_cols|length > 0 %}
+    {%- for col, col_ind in zip(context_cols, range(context_cols|length)) -%}
+    {%- for key, type in zip(context_keys[col_ind], context_types[col_ind]) -%}
+    {% if context_aliases|length > 0 -%}
+    , {{ col }}[SAFE_OFFSET(0)]:{{ key }} as {{ context_aliases[col_ind] }}_{{ key }}
+    {% else -%}
+    , {{ col }}[SAFE_OFFSET(0)].{{ key }} as {{ key }}
+    {%- endif -%}
+    {%- endfor -%}
+    {%- endfor -%}
+    {%- endif %}
+from
+    {{ ref('snowplow_web_base_events_this_run') }}
+where
+    event_name = '{{ event_name }}'
+    and {{ snowplow_utils.is_run_with_new_events("snowplow_web") }}
+{% endmacro %} #}
---
+++ macros/users_table.sql
@@ -0,0 +1,74 @@
+{% macro users_table(user_cols = [], user_keys = [], user_types = []) %}
+    {{ return(adapter.dispatch('users_table')(user_cols, user_keys, user_types)) }}
+{% endmacro %}
+
+{% macro snowflake__users_table(user_cols, user_keys, user_types) %}
+
+{# Remove down to major version for Snowflake columns, drop 2 last _X values #}
+{%- set user_cols_clean = [] -%}
+{%- for ind in range(user_cols|length) -%}
+    {% do user_cols_clean.append('_'.join(user_cols[ind].split('_')[:-2])) -%} 
+{%- endfor -%}
+
+select
+    user_id
+    , collector_tstamp as latest_collector_tstamp
+    -- user column(s) from the event table
+    {% if user_cols_clean|length > 0 %}
+    {%- for col, col_ind in zip(user_cols_clean, range(user_cols_clean|length)) -%}
+    {%- for key, type in zip(user_keys[col_ind], user_types[col_ind]) -%}
+    , {{ col }}[0]:{{ key }}::{{ type }} as {{ key }}
+    {% endfor -%}
+    {%- endfor -%}
+    {%- endif %}
+from
+    {{ ref('snowplow_web_base_events_this_run') }}
+where
+    user_id is not null
+    and {{ snowplow_utils.is_run_with_new_events("snowplow_web") }}
+qualify 
+    row_number() over (partition by user_id order by collector_tstamp desc) = 1
+{% endmacro %}
+
+
+{% macro bigquery__users_table(user_cols, user_keys, user_types) %}
+{# Replace keys with snake_case where needed #}
+{% set re = modules.re %}
+{% set camel_string = '(?<!^)(?=[A-Z])'%}
+{%- set user_keys_clean = [] -%}
+{%- for ind1 in range(user_keys|length) -%}
+    {%- set user_key_clean = [] -%}
+    {%- for ind2 in range(user_keys[ind1]|length) -%}
+        {% do user_key_clean.append(re.sub(camel_string, '_', user_keys[ind1][ind2]).lower()) -%} 
+    {%- endfor -%}
+    {% do user_keys_clean.append(user_key_clean) -%} 
+{%- endfor -%}
+
+
+with users_ordered as (
+select
+    user_id
+    , collector_tstamp as latest_collector_tstamp
+    -- user column(s) from the event table
+    {% if user_cols|length > 0 %}
+    {%- for col, col_ind in zip(user_cols, range(user_cols|length)) -%}
+    {%- for key in user_keys_clean[col_ind] -%}
+    , {{ col }}[SAFE_OFFSET(0)].{{ key }} as {{ key }}
+    {% endfor -%}
+    {%- endfor -%}
+    {%- endif %}
+    , row_number() over (partition by user_id order by collector_tstamp desc) as rn
+from
+    {{ ref('snowplow_web_base_events_this_run') }}
+where
+    user_id is not null
+    and {{ snowplow_utils.is_run_with_new_events("snowplow_web") }}
+)
+
+select
+    * except (rn)
+from
+    users_ordered
+where
+    rn = 1
+{% endmacro %}
