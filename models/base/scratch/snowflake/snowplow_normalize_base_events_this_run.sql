3b8b080324e1ecb1cb590ff72eb72c235195455b
Ryan Hill <r.l.hill128@gmail.com>
2023-03-15 10:01:12 +0000

    Allow disabling of days late limit

--- models/base/scratch/snowflake/snowplow_normalize_base_events_this_run.sql
+++ models/base/scratch/snowflake/snowplow_normalize_base_events_this_run.sql
@@ -13,9 +13,15 @@ select
 from {{ var('snowplow__events') }} as a
 
 
-where a.dvce_sent_tstamp <= {{ snowplow_utils.timestamp_add('day', var("snowplow__days_late_allowed", 3), 'a.dvce_created_tstamp') }}
-and a.collector_tstamp >= {{ lower_limit }}
-and a.collector_tstamp <= {{ upper_limit }}
-and {{ snowplow_utils.app_id_filter(var("snowplow__app_id",[])) }}
+where
+  {# dvce_sent_tstamp is an optional field and not all trackers/webhooks populate it, this means this filter needs to be optional #}
+    {% if var("snowplow__days_late_allowed") == -1 %}
+      1 = 1
+    {% else %}
+      a.dvce_sent_tstamp <= {{ snowplow_utils.timestamp_add('day', var("snowplow__days_late_allowed", 3), 'a.dvce_created_tstamp') }}
+    {% endif %}
+    and a.collector_tstamp >= {{ lower_limit }}
+    and a.collector_tstamp <= {{ upper_limit }}
+    and {{ snowplow_utils.app_id_filter(var("snowplow__app_id",[])) }}
 
 qualify row_number() over (partition by a.event_id order by a.collector_tstamp) = 1
