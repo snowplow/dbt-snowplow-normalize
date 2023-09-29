02a870210096303ef17e9c19027ddb4d06ef4e5a
Emiel <emiel.verkade@gmail.com>
2023-09-29 11:53:30 +0200

    Implement the base macro (where possible)

--- models/base/scratch/snowplow_normalize_base_new_event_limits.sql
+++ models/base/scratch/snowplow_normalize_base_new_event_limits.sql
@@ -10,14 +10,15 @@
 {% set min_last_success,
          max_last_success,
          models_matched_from_manifest,
-         has_matched_all_models = snowplow_utils.get_incremental_manifest_status(ref('snowplow_normalize_incremental_manifest'), models_in_run) -%}
+         has_matched_all_models = snowplow_utils.get_incremental_manifest_status(ref('snowplow_normalize_incremental_manifest'),
+                                                                                 models_in_run) -%}
 
 
 {% set run_limits_query = snowplow_utils.get_run_limits(min_last_success,
-                                                          max_last_success,
-                                                          models_matched_from_manifest,
-                                                          has_matched_all_models,
-                                                          var("snowplow__start_date","2020-01-01")) -%}
+                                                         max_last_success,
+                                                         models_matched_from_manifest,
+                                                         has_matched_all_models,
+                                                         var("snowplow__start_date","2020-01-01")) -%}
 
 
 {{ run_limits_query }}
