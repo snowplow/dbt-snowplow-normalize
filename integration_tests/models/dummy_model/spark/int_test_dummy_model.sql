2ab462b62c03b9093fb7bdfa07826fbccdc6b79c
Ilias Xenogiannis <ilias1111@users.noreply.github.com>
2024-11-14 17:30:12 +0200

    Prepare the codebase for spark release (#46)
    Correct naming
    
    Add ci for spark
    
    A rename for integration_test file

---
+++ integration_tests/models/dummy_model/spark/int_test_dummy_model.sql
@@ -0,0 +1,12 @@
+{#
+Copyright (c) 2022-present Snowplow Analytics Ltd. All rights reserved.
+This program is licensed to you under the Snowplow Personal and Academic License Version 1.0,
+and you may not use this file except in compliance with the Snowplow Personal and Academic License Version 1.0.
+You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 at https://docs.snowplow.io/personal-and-academic-license-1.0/
+#}
+
+{{ config(
+    tags = "snowplow_normalize_incremental",
+) }}
+
+select 1 as dummy from {{ ref ('snowplow_normalize_base_events_this_run')}}
