f8521e339ffa78329ca114bc10d6a76893b9932c
Ilias Xenogiannis <ilias1111@users.noreply.github.com>
2024-12-03 15:40:44 +0200

    Pe 6540 bq new loader (#48)

--- integration_tests/models/dummy_model/bigquery/snowplow_normalize_stg.sql
+++ integration_tests/models/dummy_model/bigquery/snowplow_normalize_stg.sql
@@ -8,21 +8,23 @@ You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 
 with prep as (
 select
   *
-  except(contexts_test_1_0_0,contexts_test2_1_0_0,contexts_test2_1_0_1,contexts_test2_1_0_2,contexts_test2_1_0_3,contexts_test2_1_0_4,contexts_test2_1_0_5),
+  except(contexts_test_1_0_0,contexts_test2_1_0_0,contexts_test2_1_0_1,contexts_test2_1_0_2,contexts_test2_1_0_3,contexts_test2_1_0_4,contexts_test2_1_0_5, contexts_test4_1,contexts_test4_2),
   JSON_EXTRACT_ARRAY(contexts_test_1_0_0) AS contexts_test_1_0_0,
   JSON_EXTRACT_ARRAY(contexts_test2_1_0_0) AS contexts_test2_1_0_0,
   JSON_EXTRACT_ARRAY(contexts_test2_1_0_1) AS contexts_test2_1_0_1,
   JSON_EXTRACT_ARRAY(contexts_test2_1_0_2) AS contexts_test2_1_0_2,
   JSON_EXTRACT_ARRAY(contexts_test2_1_0_3) AS contexts_test2_1_0_3,
   JSON_EXTRACT_ARRAY(contexts_test2_1_0_4) AS contexts_test2_1_0_4,
-  JSON_EXTRACT_ARRAY(contexts_test2_1_0_5) AS contexts_test2_1_0_5
+  JSON_EXTRACT_ARRAY(contexts_test2_1_0_5) AS contexts_test2_1_0_5,
+  JSON_EXTRACT_ARRAY(contexts_test4_1) AS contexts_test4_1,
+  JSON_EXTRACT_ARRAY(contexts_test4_2) AS contexts_test4_2
 
 from {{ ref('snowplow_norm_dummy_events') }}
 )
 
 -- recreate repeated record field i.e. array of structs as is originally in BQ events table
 select
-  * except(unstruct_event_test_1_0_0,unstruct_event_test_1_0_1,unstruct_event_test2_1_0_0,unstruct_event_test2_1_0_1,contexts_test_1_0_0,contexts_test2_1_0_0,contexts_test2_1_0_1,contexts_test2_1_0_2,contexts_test2_1_0_3,contexts_test2_1_0_4,contexts_test2_1_0_5),
+  * except(unstruct_event_test_1_0_0,unstruct_event_test_1_0_1,unstruct_event_test2_1_0_0,unstruct_event_test2_1_0_1,contexts_test_1_0_0,contexts_test2_1_0_0,contexts_test2_1_0_1,contexts_test2_1_0_2,contexts_test2_1_0_3,contexts_test2_1_0_4,contexts_test2_1_0_5,contexts_test4_1,contexts_test4_2),
     -- order is reversed to test the aliasing of the coalesced columns
     struct(JSON_EXTRACT_scalar(unstruct_event_test_1_0_0, '$.test_class') as test_class,JSON_EXTRACT_scalar(unstruct_event_test_1_0_0, '$.test_id') as test_id) as unstruct_event_test_1_0_0,
     struct(JSON_EXTRACT_scalar(unstruct_event_test_1_0_1, '$.test_class') as test_class,JSON_EXTRACT_scalar(unstruct_event_test_1_0_1, '$.test_id') as test_id) as unstruct_event_test_1_0_1,
@@ -56,7 +58,15 @@ select
     array(
     select struct(JSON_EXTRACT_scalar(json_array,'$.context_test_class2') as context_test_class2, JSON_EXTRACT_scalar(json_array,'$.context_test_id2') as context_test_id2)
     from unnest(contexts_test2_1_0_5) as json_array
-    ) as contexts_test2_1_0_5
+    ) as contexts_test2_1_0_5,
+    array(
+    select struct(JSON_EXTRACT_scalar(json_array,'$.context_test_class2') as context_test_class2, JSON_EXTRACT_scalar(json_array,'$.context_test_id2') as context_test_id2)
+    from unnest(contexts_test4_1) as json_array
+    ) as contexts_test4_1,
 
-
+    array(
+    select struct(JSON_EXTRACT_scalar(json_array,'$.context_test_class2') as context_test_class2, JSON_EXTRACT_scalar(json_array,'$.context_test_id2') as context_test_id2)
+    from unnest(contexts_test4_2) as json_array
+    ) as contexts_test4_2
+  
 from prep
