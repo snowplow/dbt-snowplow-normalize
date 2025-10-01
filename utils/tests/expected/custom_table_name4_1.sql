7da3ad1c66e18a72576560e22aaa12e70a3afb90
Agnes Kiss <agnes@snowplowanalytics.com>
2025-10-01 09:12:05 +0100

    Remove rename_partition_tstamp_date macro

--- utils/tests/expected/custom_table_name4_1.sql
+++ utils/tests/expected/custom_table_name4_1.sql
@@ -6,7 +6,7 @@
     partition_by = snowplow_utils.get_value_by_target_type(bigquery_val={
       "field":  var("snowplow__partition_tstamp"),
       "data_type": "timestamp"
-    }, databricks_val=rename_partition_tstamp_date()),
+    }, databricks_val=var('snowplow__partition_tstamp')~"_date"),
     sql_header=snowplow_utils.set_query_tag(var('snowplow__query_tag', 'snowplow_dbt')),
     tblproperties={
       'delta.autoOptimize.optimizeWrite' : 'true',
